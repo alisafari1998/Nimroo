aa32bc56c06c8e9201c3ffd296d107610250aae9
ArminF1996 <armin.f1996@gmail.com>
2018-07-24 20:43:23 +0430

    add Language class. develop detector method for Language class. add test for Language detector method. test performance of detector method.

--- src/main/java/ir/sahab/nimroo/view/App.java
+++ src/main/java/ir/sahab/nimroo/view/App.java
@@ -1,16 +1,9 @@
 package ir.sahab.nimroo.view;
 
-import ir.sahab.nimroo.model.Language;
-import java.io.IOException;
-
 /** Hello world! */
 public class App {
+
   public static void main(String[] args) {
     System.out.println("Hello World!");
-    try {
-      Language.getInstance().init();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
   }
 }
