aa978d36302ead52c20a4f2d146c6781c17e4023
omidhap <ohajabdolpoor@gmail.com>
2018-07-31 17:39:30 +0430

    elasticsearch client index as bulk

--- src/main/java/ir/sahab/nimroo/model/ElasticClient.java
+++ src/main/java/ir/sahab/nimroo/model/ElasticClient.java
@@ -1,33 +1,45 @@
 package ir.sahab.nimroo.model;
 
 import org.apache.http.HttpHost;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.xcontent.XContentBuilder;
-import org.elasticsearch.common.xcontent.XContentFactory;
 
 import java.io.IOException;
+
+import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
+
 public class ElasticClient {
   RestHighLevelClient client;
+  BulkRequest request;
 
   public ElasticClient() {
     client = new RestHighLevelClient(RestClient.builder(new HttpHost("localhost", 9200, "http")));
+    request = new BulkRequest();
   }
 
-  public void postToElastic(String title, String text, String anchor, String index, String description, String keywords) throws IOException {
-    XContentBuilder builder = XContentFactory.jsonBuilder();
-    builder.startObject();
-    {
-      builder.field("title", title);
-      builder.field("text", text);
-      builder.field("anchor", anchor);
-      builder.field("description" , description);
-      builder.field("keywords" , keywords);
+  public void postToElastic(
+      String title, String text, String anchor, String index, String description, String keywords)
+      throws IOException {
+    XContentBuilder builder =
+        jsonBuilder()
+            .startObject()
+            .field("title", title)
+            .field("text", text)
+            .field("anchor", anchor)
+            .field("description", description)
+            .field("keywords", keywords)
+            .endObject();
+    request.add(new IndexRequest(index, "_doc").source(builder));
+  }
+
+  public void addBulkToElastic() throws IOException {
+    if (request.numberOfActions() > 0) {
+      client.bulk(request);
+      request = new BulkRequest();
     }
-    builder.endObject();
-    IndexRequest indexRequest = new IndexRequest(index, "_doc");
-    indexRequest.source(builder);
-    client.index(indexRequest);
   }
 }
