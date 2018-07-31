9f2bce593b3682b49382018beee24280c3d77a47
omidhap <ohajabdolpoor@gmail.com>
2018-07-31 14:59:13 +0430

    elasticsearch client added

---
+++ src/main/java/ir/sahab/nimroo/model/ElasticClient.java
@@ -0,0 +1,32 @@
+package ir.sahab.nimroo.model;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
+
+import java.io.IOException;
+public class ElasticClient {
+  RestHighLevelClient client;
+
+  public ElasticClient() {
+    client = new RestHighLevelClient(RestClient.builder(new HttpHost("localhost", 9200, "http")));
+  }
+
+  public void postToElastic(String title, String text, String anchor, String index, String description) throws IOException {
+    XContentBuilder builder = XContentFactory.jsonBuilder();
+    builder.startObject();
+    {
+      builder.field("title", title);
+      builder.field("text", text);
+      builder.field("anchor", anchor);
+      builder.field("description" , description);
+    }
+    builder.endObject();
+    IndexRequest indexRequest = new IndexRequest(index, "_doc");
+    indexRequest.source(builder);
+    client.index(indexRequest);
+  }
+}
