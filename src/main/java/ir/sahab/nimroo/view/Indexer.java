74cb38529841bdcb29c70460bf90b3f189a23051
omidhap <ohajabdolpoor@gmail.com>
2018-08-04 16:22:37 +0430

    indexer fixed config

--- src/main/java/ir/sahab/nimroo/view/Indexer.java
+++ src/main/java/ir/sahab/nimroo/view/Indexer.java
@@ -1,13 +1,13 @@
 package ir.sahab.nimroo.view;
 
 import com.google.protobuf.InvalidProtocolBufferException;
+import ir.sahab.nimroo.Config;
 import ir.sahab.nimroo.controller.StoreManager;
 
 public class Indexer {
   public static void main(String[] args) throws InvalidProtocolBufferException, InterruptedException {
-
+      Config.load();
       StoreManager storeManager = new StoreManager();
       storeManager.start();
-
   }
 }
