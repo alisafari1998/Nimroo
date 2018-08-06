a83bdec2210946494653f1fc6e0943cd28a786bd
ArminF1996 <armin.f1996@gmail.com>
2018-08-06 14:22:05 +0430

    add HBase class and some method. add test for duplicated link in hbase.

---
+++ src/main/java/ir/sahab/nimroo/hbase/HBase.java
@@ -0,0 +1,123 @@
+package ir.sahab.nimroo.hbase;
+
+import static org.apache.hadoop.hbase.util.Bytes.toBytes;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Admin;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+public class HBase {
+
+  private static HBase ourInstance = new HBase();
+  public static HBase getInstance() {
+    return ourInstance;
+  }
+  private static final Logger logger = Logger.getLogger(HBase.class);
+  private Configuration config;
+  private String hbaseSitePath;
+  private String coreSitePath;
+  private String linksFamily;
+  private String pageDataFamily;
+  private String pageRankFamily;
+  private String tableName;
+
+  private HBase() {
+    String appConfigPath = "app.properties";
+    Properties properties = new Properties();
+    try (FileInputStream fis = new FileInputStream(appConfigPath)) {
+      properties.load(fis);
+      coreSitePath = properties.getProperty("core.site.path");
+      hbaseSitePath = properties.getProperty("hbase.site.path");
+    }catch (IOException e){
+      e.printStackTrace();
+    }
+    coreSitePath = "/home/hadoop/etc/hadoop/core-site.xml";
+    hbaseSitePath = "/home/hadoop/HBase/hbase-1.2.6.1/conf/hbase-site.xml";
+    PropertyConfigurator.configure("log4j.properties");
+    config = HBaseConfiguration.create();
+    config.addResource(new Path(hbaseSitePath));
+    config.addResource(new Path(coreSitePath));
+    tableName = "nimroo";
+    linksFamily = "links";
+    pageDataFamily = "pageData";
+    pageRankFamily = "pageRank";
+    try {
+      createTable();
+    } catch (IOException e) {
+      logger.error("possibly we can not get admin from HBase connection!", e);
+    }
+  }
+
+  private Admin getAdmin(){
+    try{
+      return ConnectionFactory.createConnection(config).getAdmin();
+    }catch (IOException e){
+      return null;
+    }
+  }
+
+  public void setHbaseSitePath(String hbaseSitePath) {
+    this.hbaseSitePath = hbaseSitePath;
+  }
+
+  public void setCoreSitePath(String coreSitePath) {
+    this.coreSitePath = coreSitePath;
+  }
+
+  public void createTable() throws IOException {
+    Admin admin = getAdmin();
+    if(admin == null)
+      return;
+    if (admin.tableExists(TableName.valueOf(tableName))) {
+      return;
+    }
+    HTableDescriptor tableDescriptor = new HTableDescriptor(TableName.valueOf(tableName));
+    tableDescriptor.addFamily(new HColumnDescriptor(linksFamily));
+    tableDescriptor.addFamily(new HColumnDescriptor(pageDataFamily));
+    tableDescriptor.addFamily(new HColumnDescriptor(pageRankFamily));
+    admin.createTable(tableDescriptor);
+  }
+
+  public void dropTable(String tableName) throws IOException {
+    Admin admin = getAdmin();
+    if(admin == null)
+      return;
+    if (!admin.tableExists(TableName.valueOf(tableName))) {
+      return;
+    }
+    admin.disableTable(TableName.valueOf(tableName));
+    admin.deleteTable(TableName.valueOf(tableName));
+  }
+
+  public boolean isDuplicateUrl(String link){
+
+    try {
+      HTable table = new HTable(config, "nimroo");
+      Get get = new Get(toBytes(link)).addColumn(toBytes("links"),toBytes("link"));
+      if(table.get(get).isEmpty()){
+        Put p = new Put(toBytes(link));
+        p.addColumn(toBytes(linksFamily), toBytes("link"), toBytes(link));
+        table.put(p);
+        return false;
+      }
+      return true;
+    } catch (IOException e) {
+      logger.warn("some exception happen in duplicateUrl method!" + e);
+      return false;
+    }
+  }
+}
---
+++ src/test/java/ir/sahab/nimroo/hbase/HBaseTest.java
@@ -0,0 +1,27 @@
+package ir.sahab.nimroo.hbase;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HBaseTest {
+
+  @Test
+  public void addUrlTest() {
+    try {
+      HBase.getInstance().dropTable("nimroo");
+      HBase.getInstance().createTable();
+    } catch (IOException e) {
+      return;
+    }
+    String url1 = "http://www.corejavaguru.com/bigdata/hbase-tutorial/hbase-java-client-api-examples";
+    String url2 = "https://community.hortonworks.com/questions/143849/how-to-retrieve-a-particular-rowkey-from-hbase-tab.html";
+    String url3 = "https://community.hortonworks.com/questions/143849/how-to-retrieve-a-particular-rowkey-from-hbase-tab";
+    Assert.assertFalse(HBase.getInstance().isDuplicateUrl(url1));
+    Assert.assertFalse(HBase.getInstance().isDuplicateUrl(url2));
+    Assert.assertTrue(HBase.getInstance().isDuplicateUrl(url1));
+    Assert.assertFalse(HBase.getInstance().isDuplicateUrl(url3));
+  }
+}
