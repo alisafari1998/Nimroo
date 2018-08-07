63bd6431506451c36427b3937ad1856ef166fcb3
ArminF1996 <armin.f1996@gmail.com>
2018-08-07 15:27:04 +0430

    change Indexer main method for store data to Hbase.

--- src/main/java/ir/sahab/nimroo/view/Indexer.java
+++ src/main/java/ir/sahab/nimroo/view/Indexer.java
@@ -3,12 +3,14 @@ package ir.sahab.nimroo.view;
 import ir.sahab.nimroo.Config;
 import ir.sahab.nimroo.controller.StoreManager;
 
+import ir.sahab.nimroo.hbase.HBase;
 import java.io.IOException;
 
 public class Indexer {
   public static void main(String[] args) throws IOException, InterruptedException {
       Config.load();
-      StoreManager storeManager = new StoreManager();
-      storeManager.start();
+//      StoreManager storeManager = new StoreManager();
+//      storeManager.start();
+    HBase.getInstance().storeFromKafka();
   }
 }
