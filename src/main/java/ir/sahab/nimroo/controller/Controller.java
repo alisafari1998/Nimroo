f23ba451a6ae80886511db631948284af637e697
alisafari <alisafar1212@gmail.com>
2018-08-08 12:41:31 +0430

    Add some logs

--- src/main/java/ir/sahab/nimroo/controller/Controller.java
+++ src/main/java/ir/sahab/nimroo/controller/Controller.java
@@ -2,7 +2,6 @@ package ir.sahab.nimroo.controller;
 
 import ir.sahab.nimroo.Config;
 import ir.sahab.nimroo.connection.HttpRequest;
-import ir.sahab.nimroo.connection.NewHttpRequest;
 import ir.sahab.nimroo.hbase.HBase;
 import ir.sahab.nimroo.kafka.KafkaHtmlProducer;
 import ir.sahab.nimroo.kafka.KafkaLinkConsumer;
@@ -11,11 +10,11 @@ import ir.sahab.nimroo.model.*;
 import ir.sahab.nimroo.parser.HtmlParser;
 import ir.sahab.nimroo.serialization.PageDataSerializer;
 import javafx.util.Pair;
-import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.log4j.Logger;
 
 import java.util.*;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class Controller {
 
@@ -33,9 +32,11 @@ public class Controller {
     private final DummyUrlCache dummyUrlCache = new DummyUrlCache();
 
     private Logger logger = Logger.getLogger(Controller.class);
-    private Long count = 0L, rejectByLRU = 0L;
+    private Long rejectByLRU = 0L;
+    private AtomicLong count = new AtomicLong(0L);
     private ExecutorService executorService;
-
+    private int allLinksCount = 1;
+    private double passedDomainCheckCount, passedPageCheckCount;
     public void start() throws InterruptedException {
         HttpRequest.init();
         long time = System.currentTimeMillis(),timeLru, timeProduceBack;
@@ -43,9 +44,11 @@ public class Controller {
             logger.info("Start to poll");
             ArrayList<String> links = kafkaLinkConsumer.get();
             logger.info("End to poll");
+            logger.info("Summery kafkaConsumedBatchSize: " + links.size());
+
             for (int i = 0; i < links.size();) {
                 String link = links.get(i);
-
+                allLinksCount++;
                 timeLru = System.currentTimeMillis();
                 if (!dummyDomainCache.add(link, System.currentTimeMillis())) {
                     rejectByLRU++;
@@ -56,20 +59,22 @@ public class Controller {
                     i++;
                     continue;
                 }
-
+                passedDomainCheckCount++;
                 if (!dummyUrlCache.add(link) || HBase.getInstance().isDuplicateUrl(link)) {
                     i++;
                     continue;
                 }
-
+                passedPageCheckCount++;
                 timeLru = System.currentTimeMillis() - timeLru;
                 logger.info("[Timing] TimeLru: " + timeLru);
 
                 try {
                     executorService.submit(()-> crawl(link, "KafkaLinkConsumer"));
 
-                    logger.info("Summery count: " + count + " speedM: " + 60 *  count / ((System.currentTimeMillis() - time) / 1000));
-                    logger.info("Summery count: " + count + " speedS: " + count / ((System.currentTimeMillis() - time) / 1000));
+                    logger.info("Summery count: " + count + " speedM: " + 60 *  count.longValue() / ((System.currentTimeMillis() - time) / 1000));
+                    logger.info("Summery count: " + count + " speedS: " + count.longValue() / ((System.currentTimeMillis() - time) / 1000));
+                    logger.info("Summery allLinks: " + allLinksCount + " passedDomain: " + passedDomainCheckCount / allLinksCount * 100
+                            + "% passedPageCheck: " + passedPageCheckCount / allLinksCount * 100 + "%");
                     logger.info("domains: " + dummyDomainCache.size());
                     logger.info("rejectionsByLRU: " + rejectByLRU);
 
@@ -116,9 +121,6 @@ public class Controller {
             return;
         }
 
-
-
-
         PageData pageData = null;
         timeParse = System.currentTimeMillis();
         htmlParser = new HtmlParser();
@@ -144,6 +146,7 @@ public class Controller {
 
         timeProducePageData = System.currentTimeMillis();
         kafkaHtmlProducer.send(Config.kafkaHtmlTopicName, pageData.getUrl(), bytes); //todo topic
+//        logger.info("PageData:\t" + pageData.toString());
         timeProducePageData = System.currentTimeMillis() - timeProducePageData;
         logger.info("[Timing] TimeProducePageData : " + timeProducePageData);
 
@@ -154,8 +157,6 @@ public class Controller {
         }
         timeProduceLinks = System.currentTimeMillis() - timeProduceLinks;
         logger.info("[Timing] TimeProduceLinks: " + timeProduceLinks);
-        synchronized (count) {
-            count++;
-        }
+        count.addAndGet(1);
     }
 }
