d17a915f844f89d35dee72cccf9faf38a98a53aa
omidhap <ohajabdolpoor@gmail.com>
2018-08-11 12:42:17 +0430

    new main for moving kafka topic

---
+++ src/main/java/ir/sahab/nimroo/view/NewKafkaTopic.java
@@ -0,0 +1,37 @@
+package ir.sahab.nimroo.view;
+
+import ir.sahab.nimroo.Config;
+import ir.sahab.nimroo.hbase.HBase;
+import ir.sahab.nimroo.kafka.KafkaLinkConsumer;
+import ir.sahab.nimroo.kafka.KafkaLinkProducer;
+import ir.sahab.nimroo.model.DummyUrlCache;
+
+import java.util.ArrayList;
+
+public class NewKafkaTopic {
+  private KafkaLinkConsumer kafkaLinkConsumer;
+  private KafkaLinkProducer kafkaLinkProducer;
+  private final DummyUrlCache dummyUrlCache = new DummyUrlCache();
+
+  public static void main(String[] args) {
+    Config.load();
+    NewKafkaTopic newKafkaTopic = new NewKafkaTopic();
+    newKafkaTopic.filter();
+  }
+
+  public NewKafkaTopic() {
+    kafkaLinkConsumer = new KafkaLinkConsumer();
+    kafkaLinkProducer = new KafkaLinkProducer();
+  }
+
+  private void filter() {
+    ArrayList<String> links = kafkaLinkConsumer.get();
+    for (int i = 0; i < links.size();i++) {
+      String link = links.get(i);
+      if (!dummyUrlCache.add(link) || HBase.getInstance().isDuplicateUrl(link)) {
+        continue;
+      }
+      kafkaLinkProducer.send("goodlinks", "", link);
+    }
+  }
+}
