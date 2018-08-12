cb7cd9ed53973b0aec0d688e0c6d42a97262c195
ArminF1996 <armin.f1996@gmail.com>
2018-08-12 14:21:10 +0430

    + add more useless anchor to repetitive anchors Class for map reduce.  - language detector for add page to HBase in store method HBase Class.

--- src/main/java/ir/sahab/nimroo/mapreduce/RepetitiveAnchors.java
+++ src/main/java/ir/sahab/nimroo/mapreduce/RepetitiveAnchors.java
@@ -1,12 +1,14 @@
 package ir.sahab.nimroo.mapreduce;
 
-import ir.sahab.nimroo.hbase.HBase;
 import ir.sahab.nimroo.model.Link;
 import ir.sahab.nimroo.model.PageData;
 import ir.sahab.nimroo.serialization.PageDataSerializer;
 import java.io.IOException;
 import java.util.ArrayList;
 import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.Scan;
@@ -34,6 +36,84 @@ public class RepetitiveAnchors {
     uselessAnchors.add("click");
     uselessAnchors.add("ref");
     uselessAnchors.add("here");
+    uselessAnchors.add(".");
+
+    uselessAnchors.add("january");
+    uselessAnchors.add("february");
+    uselessAnchors.add("march");
+    uselessAnchors.add("april");
+    uselessAnchors.add("may");
+    uselessAnchors.add("june");
+    uselessAnchors.add("july");
+    uselessAnchors.add("august");
+    uselessAnchors.add("september");
+    uselessAnchors.add("october");
+    uselessAnchors.add("november");
+    uselessAnchors.add("december");
+
+    uselessAnchors.add("monday");
+    uselessAnchors.add("tuesday");
+    uselessAnchors.add("wednesday");
+    uselessAnchors.add("thursday");
+    uselessAnchors.add("friday");
+    uselessAnchors.add("saturday");
+    uselessAnchors.add("sunday");
+
+
+    uselessAnchors.add("\\xe2\\x96\\xba");
+    uselessAnchors.add("older posts");
+    uselessAnchors.add("privacy policy");
+    uselessAnchors.add("links to this post");
+    uselessAnchors.add("no comments:");
+    uselessAnchors.add("comment");
+    uselessAnchors.add("comments");
+    uselessAnchors.add("skip to sidebar");
+    uselessAnchors.add("skip to main");
+    uselessAnchors.add("(3)");
+    uselessAnchors.add("(2)");
+    uselessAnchors.add("(1)");
+    uselessAnchors.add("(4)");
+    uselessAnchors.add("(5)");
+    uselessAnchors.add("(6)");
+    uselessAnchors.add("(7)");
+    uselessAnchors.add("(8)");
+    uselessAnchors.add("(9)");
+    uselessAnchors.add("(0)");
+
+    uselessAnchors.add("reply");
+    uselessAnchors.add("posts");
+    uselessAnchors.add("post");
+    uselessAnchors.add("click here");
+    uselessAnchors.add("?");
+    uselessAnchors.add("??");
+    uselessAnchors.add("???");
+    uselessAnchors.add("????");
+    uselessAnchors.add(
+        "\\xe8\\xbe\\xa3\\xe5\\xa6\\xb9\\xe7\\xbe\\x8e\\xe5\\xa5\\xb3\\xe3\\x8a\\xa3 \\xe8\\xbe\\xa3\\xe5\\xa6\\xb9\\xe7\\xbe\\x8e\\xe5\\xa5\\xb3\\xe3\\x8a\\xa3 \\xe8\\xbe\\xa3\\xe5\\xa6\\xb9\\xe7\\xbe\\x8e\\xe5\\xa5\\xb3\\xe3\\x8a\\xa3");
+    uselessAnchors.add("leave a comment");
+    uselessAnchors.add("about");
+    uselessAnchors.add("contact");
+    uselessAnchors.add("contact us");
+    uselessAnchors.add("home");
+    uselessAnchors.add("edit");
+    uselessAnchors.add("skip to content");
+    uselessAnchors.add("blog");
+    uselessAnchors.add("log in");
+    uselessAnchors.add("about us");
+    uselessAnchors.add("google");
+    uselessAnchors.add("read more");
+    uselessAnchors.add("events");
+    uselessAnchors.add("visit");
+    uselessAnchors.add("share");
+    uselessAnchors.add("report");
+    uselessAnchors.add("help");
+    uselessAnchors.add("modify");
+    uselessAnchors.add("email");
+    uselessAnchors.add("terms");
+    uselessAnchors.add("download");
+    uselessAnchors.add("menu");
+    uselessAnchors.add("faq");
+    uselessAnchors.add("faqs");
   }
 
   static class Mapper extends TableMapper<Text, Text> {
@@ -43,7 +123,7 @@ public class RepetitiveAnchors {
     @Override
     public void map(ImmutableBytesWritable row, Result values, Context context) throws IOException {
       PageData pageData = PageDataSerializer.getInstance().deserialize(values.value());
-      for(Link link : pageData.getLinks())if(!uselessAnchors.contains(link.getAnchor())){
+      for(Link link : pageData.getLinks())if(!uselessAnchors.contains(link.getAnchor().toLowerCase())){
         try {
           context.write(new Text(DigestUtils.md5Hex(link.getLink())), new Text(link.getAnchor()));
         } catch (InterruptedException e) {
@@ -62,32 +142,34 @@ public class RepetitiveAnchors {
     @Override
     public void reduce(Text key, Iterable<Text> values, Context context)
         throws IOException, InterruptedException {
-      StringBuilder test = new StringBuilder();
+      StringBuilder anchor = new StringBuilder();
       for (Text val : values) {
-        test.append(val);
-        test.append(" ");
+        anchor.append(val);
+        anchor.append(" ");
       }
       Put put = new Put(key.getBytes());
-      put.addColumn(Bytes.toBytes("test"), Bytes.toBytes("anchors"), Bytes.toBytes(test.toString()));
+      put.addColumn(Bytes.toBytes("pageDate"), Bytes.toBytes("anchors"), Bytes.toBytes(anchor.toString()));
       context.write(key, put);
     }
   }
 
   public static void main(String[] args) throws Exception {
-    Job job = new Job(HBase.getInstance().getConfig(), "Repetitive Anchors");
+    Configuration config = HBaseConfiguration.create();
+    config.addResource(new Path("/home/hadoop/hbase-1.2.6.1/conf/hbase-site.xml"));
+    config.addResource(new Path("/home/hadoop/hadoop-2.9.1/etc/hadoop/core-site.xml"));
+    Job job = new Job(config, "Repetitive Anchors");
     job.setJarByClass(RepetitiveAnchors.class);
 
 
     Scan scan = new Scan();
     scan.setCaching(500);        // 1 is the default in Scan, which will be bad for MapReduce jobs
     scan.setCacheBlocks(false);
-    scan.setStopRow(Bytes.toBytes("00000ef7231ffc5c50df"));
     scan.addColumn(Bytes.toBytes("pageData"), Bytes.toBytes("pageData"));
     scan.addColumn(Bytes.toBytes("pageData"), Bytes.toBytes("myPageData"));
 
     TableMapReduceUtil
         .initTableMapperJob("nimroo", scan, RepetitiveAnchors.Mapper.class, Text.class, Text.class, job);
-    TableMapReduceUtil.initTableReducerJob("result", RepetitiveAnchors.Reducer.class, job);
+    TableMapReduceUtil.initTableReducerJob("nimroo", RepetitiveAnchors.Reducer.class, job);
     System.exit(job.waitForCompletion(true) ? 0 : 1);
   }
 
