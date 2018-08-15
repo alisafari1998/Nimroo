59eaa579cabeda63b6ac8b539d657d28b5a34f5f
sina <taslimisina@yahoo.com>
2018-08-15 09:52:31 +0430

    Added test for pagerank

--- src/main/java/ir/sahab/nimroo/pagerank/PageRank.java
+++ src/main/java/ir/sahab/nimroo/pagerank/PageRank.java
@@ -2,14 +2,15 @@ package ir.sahab.nimroo.pagerank;
 
 import org.apache.spark.api.java.JavaPairRDD;
 
-import scala.Serializable;
+import org.apache.spark.api.java.Optional;
 import scala.Tuple2;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class PageRank implements Serializable {
-	private final Double d = 0.85;
+public class PageRank {
+	private static final double d = 0.85;
+	private static final double dComp = 0.15;
 
 	public JavaPairRDD<String, Tuple2<Double, List<String>>> calcPageRank(JavaPairRDD<String, Tuple2<Double, List<String>>> sourceRankAndLinks) {
 		JavaPairRDD<String, Double> sinkRankRDD = sourceRankAndLinks.flatMapToPair(string_doubleListString -> {
@@ -29,9 +30,20 @@ public class PageRank implements Serializable {
 				new Tuple2<>(string_doubleListString._1, string_doubleListString._2._2));
 
 		JavaPairRDD<String, Double> reducedSinkRankRDD = sinkRankRDD.reduceByKey((a, b) -> a + b);
+
 		JavaPairRDD<String, Double> finalSinkRank = reducedSinkRankRDD.mapToPair(sinkAndSum ->
 				new Tuple2<>(sinkAndSum._1, (d * sinkAndSum._2) + (1 - d)));
 
-		return finalSinkRank.join(sourceAndLinks);
+		JavaPairRDD<String, Tuple2<Optional<Double>, List<String>>> finalSourceRankLinks = finalSinkRank.rightOuterJoin(sourceAndLinks);
+		return finalSourceRankLinks.mapToPair(newSourceRankLink -> {
+			Optional<Double> optionalDouble = newSourceRankLink._2._1;
+			if (optionalDouble.isPresent()) {
+				return new Tuple2<>(newSourceRankLink._1, new Tuple2<>(optionalDouble.get(), newSourceRankLink._2._2));
+			}
+			else {
+				return new Tuple2<>(newSourceRankLink._1, new Tuple2<>(dComp, newSourceRankLink._2._2));
+			}
+		});
+//		return finalSinkRank.join(sourceAndLinks);
 	}
 }
