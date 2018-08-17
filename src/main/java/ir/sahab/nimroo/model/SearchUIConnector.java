e0acfd7c140aead676e43df2b0efe02b7f00b4d9
omidhap <ohajabdolpoor@gmail.com>
2018-08-17 12:36:27 +0430

    a connector for elastic client and UI

---
+++ src/main/java/ir/sahab/nimroo/model/SearchUIConnector.java
@@ -0,0 +1,51 @@
+package ir.sahab.nimroo.model;
+
+import ir.sahab.nimroo.hbase.HBase;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class SearchUIConnector {
+  private ElasticClient elasticClient;
+
+  public SearchUIConnector() {
+    elasticClient = new ElasticClient();
+  }
+
+  public HashMap<String, Double> simpleSearch(
+      String searchText, String index, boolean safety, boolean pageRank) throws IOException {
+    elasticClient.setSafeSearch(safety);
+    ArrayList<String> ans = elasticClient.simpleSearchInElasticForWebPage(searchText, index);
+    return makeHashMap(ans,pageRank);
+  }
+
+  public HashMap<String, Double> advancedSearch(
+      ArrayList<String> mustFind,
+      ArrayList<String> mustNotFind,
+      ArrayList<String> shouldFind,
+      String index,
+      boolean safety,
+      boolean pageRank)
+      throws IOException {
+    elasticClient.setSafeSearch(safety);
+    ArrayList<String> ans = elasticClient.advancedSearchInElasticForWebPage(mustFind,mustNotFind,shouldFind, index);
+    return makeHashMap(ans,pageRank);
+  }
+
+  private HashMap<String, Double> makeHashMap(ArrayList<String> links,boolean pageRank){
+      if (!pageRank) {
+          HashMap<String, Double> map = new HashMap<>();
+          for (String temp : links) {
+              map.put(temp, null);
+          }
+          return map;
+      } else {
+          HashMap<String, Double> map = new HashMap<>();
+          for (String temp : links) {
+              map.put(temp, HBase.getInstance().getPageRank(temp));
+          }
+          return map;
+      }
+  }
+}
