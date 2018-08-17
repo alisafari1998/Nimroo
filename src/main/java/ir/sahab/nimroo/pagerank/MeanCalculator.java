5f0a8a9b56a019bd3f41be5f2c0928b0607f76f1
sina <taslimisina@yahoo.com>
2018-08-17 16:50:05 +0430

    Moved pagerank data from other tables to 'nimroo' in HBase

--- src/main/java/ir/sahab/nimroo/pagerank/MeanCalculator.java
+++ src/main/java/ir/sahab/nimroo/pagerank/MeanCalculator.java
@@ -4,16 +4,22 @@ import ir.sahab.nimroo.Config;
 import ir.sahab.nimroo.view.TestSmallPageRank;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 import org.apache.hadoop.hbase.mapreduce.TableInputFormat;
+import org.apache.hadoop.hbase.mapreduce.TableOutputFormat;
 import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
+import java.io.IOException;
 
 public class MeanCalculator {
 	private static Configuration hBaseConfiguration = null;
@@ -49,6 +55,37 @@ public class MeanCalculator {
 		double pageRankSum = pageRanksRDD.reduce((a, b) -> a + b);
 		long num = pageRanksRDD.count();
 
+		System.out.println(pageRankSum);
+		System.out.println(num);
 		System.out.println(pageRankSum / num);
+
+/*		Job job = null;
+		System.out.println("start configuring job");
+		try {
+			job = Job.getInstance(hBaseConfiguration);
+			job.getConfiguration().set(TableOutputFormat.OUTPUT_TABLE, "nimroo");
+			job.setOutputFormatClass(TableOutputFormat.class);
+			System.out.println("Job configured");
+		} catch (IOException e) {
+			System.out.println("Job not configured.\t" + e);
+		}
+
+		System.out.println("creating hBasePuts rdd...");
+		JavaPairRDD<ImmutableBytesWritable, Put> hBasePuts = hBaseRDD.mapToPair(pairRow -> {
+			Result result = pairRow._2;
+
+			byte[] bytes = result.getValue(Bytes.toBytes("PageRankFamily"), Bytes.toBytes("myPageRank"));
+			double myPageRank = Bytes.toDouble(bytes);
+
+			Put put = new Put(pairRow._1.get());
+			put.addColumn(Bytes.toBytes("pageRank"), Bytes.toBytes("myPageRank"), Bytes.toBytes(myPageRank));
+
+			return new Tuple2<>(new ImmutableBytesWritable(), put);
+		});
+		System.out.println("hBasePuts rdd created.");
+
+		System.out.println("saving data in HBase...");
+		hBasePuts.saveAsNewAPIHadoopDataset(job.getConfiguration());
+		System.out.println("data saved.");*/
 	}
 }
